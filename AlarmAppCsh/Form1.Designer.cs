9c61b965cf6c7e4fd8cff038e7796cbbd658cb1a
Filip Kreč <filip.krec@yahoo.com>
2018-12-11 12:32:23 +0100

    Add project files.

---
+++ AlarmAppCsh/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace AlarmAppCsh
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbClock = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.lbDate = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbClock
+            // 
+            this.lbClock.AutoSize = true;
+            this.lbClock.Font = new System.Drawing.Font("Tahoma", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbClock.Location = new System.Drawing.Point(60, 29);
+            this.lbClock.Name = "lbClock";
+            this.lbClock.Size = new System.Drawing.Size(215, 58);
+            this.lbClock.TabIndex = 0;
+            this.lbClock.Text = "00:00:00";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "UTC (London)",
+            "UTC +1 (Zagreb)",
+            "UTC +2 (Athens)",
+            "UTC +3 (Moscow)",
+            "UTC +4 (Dubai)",
+            "UTC +5  (Karachi)",
+            "UTC +6 (Dhaka)",
+            "UTC +7 (Bangkok)",
+            "UTC +8 (Beijing)",
+            "UTC +9 (Tokyo)",
+            "UTC +10 (Sydney)",
+            "UTC +11 (Noumea)",
+            "UTC + 12 (Auckland)",
+            "UTC + 13 (Phoenix Islands)",
+            "UTC + 14 (Line Islands)",
+            "UTC -1 ",
+            "UTC -2 (South Georgia)",
+            "UTC -3 (Buenos Aires)",
+            "UTC -4 (Santo Domingo)",
+            "UTC -5 (New York)",
+            "UTC -6 (Chicago)",
+            "UTC -7 (Phoenix)",
+            "UTC -8 (Los Angeles)",
+            "UTC -9 (Anchorage)",
+            "UTC - 10 (Honolulu)",
+            "UTC - 11  (American Samoa)",
+            "UTC - 12 (Howland Island)"});
+            this.comboBox1.Location = new System.Drawing.Point(70, 152);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(191, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // lbDate
+            // 
+            this.lbDate.AutoSize = true;
+            this.lbDate.Location = new System.Drawing.Point(131, 119);
+            this.lbDate.Name = "lbDate";
+            this.lbDate.Size = new System.Drawing.Size(73, 13);
+            this.lbDate.TabIndex = 2;
+            this.lbDate.Text = "00 . 00 . 0000";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(340, 228);
+            this.Controls.Add(this.lbDate);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.lbClock);
+            this.Name = "Form1";
+            this.Text = "Clock";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbClock;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label lbDate;
+    }
+}
+
---
+++ AlarmAppCsh/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AlarmAppCsh
+{
+    public partial class Form1 : Form
+    {
+        Timer t = new Timer();
+        int timeZone = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            t.Interval = 1000;
+            t.Tick += new EventHandler(this.timer);
+            t.Start();
+        }
+
+        private void timer(object sender, EventArgs e)
+        {
+            DateTime Today = DateTime.UtcNow;
+            int h = Today.Hour + timeZone;
+            if(h > 24)
+            {
+                Today = Today.AddDays(1);
+                h = h % 24;
+            }
+            else if (h < 0)
+            {
+                Today = Today.AddDays(-1);
+                h = Math.Abs(h);
+            }
+
+            string time = "";
+            time += h;
+            time += ":";
+            time += Today.Minute;
+            time += ":";
+            time += Today.Second;
+            lbClock.Text = time;
+
+            string date = "";
+            date += Today.Day;
+            date += ".";
+            date += Today.Month;
+            date += ".";
+            date += Today.Year;
+            date += ".";
+            lbDate.Text = date;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch(comboBox1.SelectedIndex)
+            {
+                case 0:
+                    timeZone = 0; break;
+                case 1:
+                    timeZone = 1; break;
+                case 2:
+                    timeZone = 2; break;
+                case 3:
+                    timeZone = 3; break; 
+                case 4:
+                    timeZone = 4; break;
+                case 5:
+                    timeZone = 5; break;
+                case 6:
+                    timeZone = 6; break;
+                case 7:
+                    timeZone = 7; break;
+                case 8:
+                    timeZone = 8; break;
+                case 9:
+                    timeZone = 9; break;
+                case 10:
+                    timeZone = 10; break;
+                case 11:
+                    timeZone = 11; break;
+                case 12:
+                    timeZone = 12; break;
+                case 13:
+                    timeZone = 13; break;
+                case 14:
+                    timeZone = 14; break;
+                case 15:
+                    timeZone = -1; break;
+                case 16:
+                    timeZone = -2; break;
+                case 17:
+                    timeZone = -3; break;
+                case 18:
+                    timeZone = -4; break;
+                case 19:
+                    timeZone = -5; break;
+                case 20:
+                    timeZone = -6; break;
+                case 21:
+                    timeZone = -7; break;
+                case 22:
+                    timeZone = -8; break;
+                case 23:
+                    timeZone = -9; break;
+                case 24:
+                    timeZone = -10; break;
+                case 25:
+                    timeZone = -11; break;
+                case 26:
+                    timeZone = -12; break;
+                default: break; 
+
+                    
+            }
+            comboBox1.SelectedValueChanged += new EventHandler(this.timer);
+        }
+    }
+}
